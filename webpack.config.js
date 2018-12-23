b3c9ae7b7303c357ed85f6ae665d7926d813a6ca
makkii_bcr <tonyu.makkii@gmail.com>
2018-12-23 13:16:54 +0900

    PicoAudio.min.jsも生成するようにした

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,39 @@
-module.exports = {
-  mode: "development",
-  //mode: 'production',
-  context: __dirname + '/src',
-  entry: {
-    'dist/PicoAudio': './main.js'
-  },
-  output: {
-    path: __dirname,
-    filename: './[name].js',
-    // library: 'PicoAudio',
-    // libraryTarget: 'var'
-  },
-  devServer: {
-    contentBase: 'dist',
-    open: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                '@babel/preset-env',
-              ]
+module.exports = (env, argv) => {
+  console.log("build mode: " + argv.mode);
+  return {
+    mode: argv.mode === 'production' ? 'production' : 'developmentl', 
+    context: __dirname + '/src',
+    entry: argv.mode === 'production' ? {
+      'dist/PicoAudio.min': './main.js'
+    } : {
+      'dist/PicoAudio': './main.js'
+    },
+    output: {
+      path: __dirname,
+      filename: './[name].js',
+      // library: 'PicoAudio',
+      // libraryTarget: 'var'
+    },
+    devServer: {
+      contentBase: 'dist',
+      open: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  '@babel/preset-env',
+                ]
+              }
             }
-          }
-        ]
-      }
-    ]
-  }
+          ]
+        }
+      ]
+    }
+  };
 };
