1b2f31f474e2c4c6d598ede910fd60a615bfcb7a
makkii_bcr <tonyu.makkii@gmail.com>
2020-08-31 00:36:38 +0900

    refactoring, add watch config

--- rollup.config.js
+++ rollup.config.js
@@ -1,73 +1,48 @@
 import babel from '@rollup/plugin-babel';
 import {terser} from 'rollup-plugin-terser';
-import serve from 'rollup-plugin-serve';
-import livereload from 'rollup-plugin-livereload';
 
 const babelParam = {
   babelHelpers: 'bundled',
   presets: ["@babel/preset-env"]
 };
 
-const isWatch = process.env.WATCH;
-
-function url() {
-  return {
-    name: 'localserver',
-    generateBundle () {
-      console.log("http://localhost:10001/sample/sample1.html");
-      console.log("http://localhost:10001/sample/sample2.html");
-    }
-  }
-}
-
-export default function () {
-  let config = [
-    {
-      input: 'src/main.js',
-      output: {
-        file: 'dist/browser/PicoAudio.js',
-        format: 'iife',
-        name: 'PicoAudio'
-      },
-      plugins: [
-        babel(babelParam),
-        // LiveReroad
-        isWatch && serve(''),
-        isWatch && livereload({watch: ['dist/browser/PicoAudio.js', 'sample']}),
-        isWatch && url()
-      ]
+export default [
+  {
+    input: 'src/main.js',
+    output: {
+      file: 'dist/browser/PicoAudio.js',
+      format: 'iife',
+      name: 'PicoAudio'
     },
-    {
-      input: 'src/main.js',
-      output: {
-        file: 'dist/browser/PicoAudio.min.js',
-        format: 'iife',
-        name: 'PicoAudio'
-      },
-      plugins: [
-        babel(babelParam),
-        terser()
-      ]
+    plugins: [
+      babel(babelParam)
+    ]
+  },
+  {
+    input: 'src/main.js',
+    output: {
+      file: 'dist/browser/PicoAudio.min.js',
+      format: 'iife',
+      name: 'PicoAudio'
     },
-    {
-      input: 'src/main.js',
-      output: [
-        {
-          file: 'dist/nodejs/pico-audio.js',
-          format: 'cjs',
-          name: 'PicoAudio',
-          exports: 'default'
-        },
-        {
-          file: 'dist/nodejs/pico-audio.mjs',
-          format: 'esm'
-        }
-      ]
-    }
-  ];
-  if (isWatch) {
-    config.pop(); // ignore pico-audio.js pico-audio.mjs
-    config.pop(); // ignore PicoAudio.min.js
+    plugins: [
+      babel(babelParam),
+      terser()
+    ]
+  },
+  {
+    input: 'src/main.js',
+    output: [
+      {
+        file: 'dist/nodejs/pico-audio.js',
+        format: 'cjs',
+        name: 'PicoAudio',
+        exports: 'default'
+      },
+      {
+        file: 'dist/nodejs/pico-audio.mjs',
+        format: 'esm'
+      }
+    ]
   }
-  return config;
-}
+];
